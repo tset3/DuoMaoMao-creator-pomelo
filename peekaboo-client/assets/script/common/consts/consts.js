2b5239e8f652f96d3bd3e91d46d25842ea411f96
jianan <jianan@happyplaygame.net>
2018-09-20 10:14:56 +0800

    fix host

--- peekaboo-client/assets/script/common/consts/consts.js
+++ peekaboo-client/assets/script/common/consts/consts.js
@@ -13,7 +13,8 @@ exp.connectorAddress =
 	// host: '192.168.1.103', //家IP
 	// host: '192.168.1.148', //公司IP
 	// host: '120.77.48.203',// 采田
-    host: 'mc.hhhhhh.org',
+	// host: 'mc.hhhhhh.org',
+	host: '127.0.0.1',
 	port: 50100,
 	log: true
 };
